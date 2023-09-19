574e7dbfdec1b728209e2c8c82a533fa6c02bfb0
Viacheslav Bocharov <adeep@lexina.in>
2023-09-19 17:10:59 +0300

    Revert "mtd-mtdsplit-support"
    This reverts commit 6a649a5005fb4acab304d577eafeb7dbf6d12dab.

--- include/linux/mtd/mtd.h
+++ include/linux/mtd/mtd.h
@@ -617,24 +617,6 @@ static inline void mtd_align_erase_req(struct mtd_info *mtd,
 		req->len += mtd->erasesize - mod;
 }
 
-static inline uint64_t mtd_roundup_to_eb(uint64_t sz, struct mtd_info *mtd)
-{
-	if (mtd_mod_by_eb(sz, mtd) == 0)
-		return sz;
-
-	/* Round up to next erase block */
-	return (mtd_div_by_eb(sz, mtd) + 1) * mtd->erasesize;
-}
-
-static inline uint64_t mtd_rounddown_to_eb(uint64_t sz, struct mtd_info *mtd)
-{
-	if (mtd_mod_by_eb(sz, mtd) == 0)
-		return sz;
-
-	/* Round down to the start of the current erase block */
-	return (mtd_div_by_eb(sz, mtd)) * mtd->erasesize;
-}
-
 static inline uint32_t mtd_div_by_ws(uint64_t sz, struct mtd_info *mtd)
 {
 	if (mtd->writesize_shift)
@@ -710,13 +692,6 @@ extern struct mtd_info *get_mtd_device_by_node(
 		const struct device_node *of_node);
 extern void put_mtd_device(struct mtd_info *mtd);
 
-static inline uint64_t mtdpart_get_offset(const struct mtd_info *mtd)
-{
-	if (!mtd_is_partition(mtd))
-		return 0;
-
-	return mtd->part.offset;
-}
 
 struct mtd_notifier {
 	void (*add)(struct mtd_info *mtd);
